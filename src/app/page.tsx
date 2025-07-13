153b9a632d4a2219cb7634a81fc003ba74b149de
Onkar Patel <onkarpatel10@gmail.com>
2025-07-13 21:16:52 +0530

    feat: reset Prisma migrations and reapply schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
-import prisma from '../lib/db';
-
-const Page = async () => {
-  const users = await prisma.user.findMany();
-  const posts = await prisma.post.findMany();
-
-  return (
-    <div>
-      {JSON.stringify(users, null, 2)} {JSON.stringify(posts, null, 2)}
-    </div>
-  );
+const Page = () => {
+  return <div>Hello World</div>;
 };
 
 export default Page;
