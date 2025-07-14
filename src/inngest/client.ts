c1c4c501c79a9e2f0c1e32d17db4b0c435c6c683
Onkar Patel <onkarpatel10@gmail.com>
2025-07-14 14:52:18 +0530

    refactor changes

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -1,4 +1,6 @@
 import { Inngest } from 'inngest';
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: 'nova-development' });
+export const inngest = new Inngest({
+  id: process.env.INNGEST_APP_ID ?? 'nova-development',
+});
