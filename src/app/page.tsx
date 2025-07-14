b5eefb412103d9468514c2ee79bef3f4a5036385
Onkar Patel <onkarpatel10@gmail.com>
2025-07-14 14:44:22 +0530

    feat: setup Inngest and enable background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,75 @@
+'use client';
+
+import { useTRPC } from '@/trpc/client';
+
+import { Button } from '@/components/ui/button';
+import { useMutation } from '@tanstack/react-query';
+import { toast } from 'sonner';
+
 const Page = () => {
-  return <div>Hello World</div>;
+  const trpc = useTRPC();
+  const invoke = useMutation(
+    trpc.invoke.mutationOptions({
+      onSuccess: () => {
+        toast.success('Background job invoked successfully!');
+      },
+    }),
+  );
+
+  return (
+    <div className='p-4 max-w-7xl mx-auto'>
+      <Button
+        disabled={invoke.isPending}
+        onClick={() => {
+          invoke.mutate({ text: 'Onkar' });
+        }}
+      >
+        Invoke Background Job
+      </Button>
+    </div>
+  );
 };
 
 export default Page;
+
+// --- Use TRPC in Server Component
+// import Client from './client';
+// import { trpc, getQueryClient } from '@/trpc/server';
+// import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
+// import { Suspense } from 'react';
+
+// // --- Use TRPC in a server component
+// const Page = async () => {
+//   const queryClient = getQueryClient();
+//   void queryClient.prefetchQuery(
+//     trpc.hello.queryOptions({ text: 'Onkar Prefetch' }),
+//   );
+
+//   return (
+//     <HydrationBoundary state={dehydrate(queryClient)}>
+//       <Suspense fallback={<p>Loading...</p>}>
+//         <Client />
+//       </Suspense>
+//     </HydrationBoundary>
+//   );
+// };
+
+// export default Page;
+
+// --- Use TRPC in a client component
+// 'use client';
+// import { useTRPC } from '@/trpc/client';
+// import { useQuery } from '@tanstack/react-query';
+
+// const Page = () => {
+//   // The API and its working (trpc)
+//   // Simple Way to fetch data using client component
+//   const trpc = useTRPC();
+//   const { data, ...other } = useQuery(
+//     trpc.hello.queryOptions({ text: 'Onkar' }),
+//   );
+
+//   return <div>{JSON.stringify(data)}</div>;
+// };
+
+// export default Page;
