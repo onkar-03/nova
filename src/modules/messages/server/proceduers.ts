fcb222587939e92a6453bb1b029739832f1387cf
Onkar Patel <onkarpatel10@gmail.com>
2025-07-17 11:15:07 +0530

    feat: Load messages for individual Project functionality added

--- src/modules/messages/server/proceduers.ts
+++ src/modules/messages/server/proceduers.ts
@@ -5,12 +5,21 @@ import { inngest } from '@/inngest/client';
 import { createTRPCRouter, baseProcedure } from '@/trpc/init';
 
 export const messagesRouter = createTRPCRouter({
-  getMany: baseProcedure.query(async () => {
-    const messages = await prisma.message.findMany({
-      orderBy: { updatedAt: 'desc' },
-    });
-    return messages;
-  }),
+  getMany: baseProcedure
+    .input(
+      z.object({
+        projectId: z.string().min(1, { message: 'Project ID is required' }),
+      }),
+    )
+    .query(async ({ input }) => {
+      const messages = await prisma.message.findMany({
+        where: {
+          projectId: input.projectId,
+        },
+        orderBy: { updatedAt: 'desc' },
+      });
+      return messages;
+    }),
   create: baseProcedure
     .input(
       z.object({
