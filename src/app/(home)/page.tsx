683b8e3cd607587c97155f5f384ae22e46c586fa
Onkar Patel <onkarpatel10@gmail.com>
2025-07-19 01:34:27 +0530

    feat: Added Projects List user Created on Home Page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,5 @@
 import ProjectForm from '@/modules/home/ui/components/project-form';
+import { ProjectsList } from '@/modules/home/ui/components/projects-list';
 import Image from 'next/image';
 const Page = () => {
   return (
@@ -23,6 +24,7 @@ const Page = () => {
           <ProjectForm />
         </div>
       </section>
+      <ProjectsList />
     </div>
   );
 };
