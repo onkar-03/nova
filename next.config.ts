f6aba320484f577cd05718dac79bc15c89af43f3
Onkar Patel <onkarpatel10@gmail.com>
2025-07-23 22:44:16 +0530

    updated next config setting to enable production build

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { isServer }) => {
-    // Exclude system directories from being watched/scanned
-    config.watchOptions = {
-      eslint: { ignoreDuringBuilds: true },
-      ...config.watchOptions,
-      ignored: [
-        '**/node_modules/**',
-        '**/.git/**',
-        '**/C:/Users/*/Cookies/**',
-        '**/C:/Users/*/AppData/**',
-        '**/C:/Windows/**',
-        '**/System32/**',
-      ],
-    };
-    return config;
-  },
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
